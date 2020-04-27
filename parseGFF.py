b127e7cde73a350aa465622754b0eb65fe79aac3
Evan Anderson <eta003@uark.edu>
2020-04-26 19:37:26 -0500

    should read exons but it is finnicky

--- parseGFF.py
+++ parseGFF.py
@@ -1,53 +1,97 @@
 #! /usr/bin/env python3
 
-# importing modules
-import csv
-from Bio import SeqIO
 import argparse
+import csv
+import re
+from Bio import SeqIO
+from collections import defaultdict
 
-# create an argument parser object
-parser = argparse.ArgumentParser(description="This script parses a GFF file and does stuff with it")
 
-# adding the required arguments to the parser 
-parser.add_argument("gff", help = "Name of the GFF file")
-parser.add_argument("fasta", help = "Name of the Fasta file")
+def get_args():
+	# create an argument parser object
+	parser = argparse.ArgumentParser(description = "This script parses a GFF file and does stuff with it")
 
-# parse the arguments
-args = parser.parse_args()
+	# add positional arguments
+	parser.add_argument("gff",   help="name of the GFF file")
+	parser.add_argument("fasta", help="name of the FASTA file")
 
-# read and parse the FASTA file 
-genome = SeqIO.read(args.fasta, 'fasta')
+	# parse the arguments
+	return parser.parse_args()
 
-# adding args.gff will hold the info parse on the command line
-with open(args.gff, "r") as gff_file: # opening the file and reading it
-        
-    #create a csv reader object
-    reader = csv.reader(gff_file, delimiter = "\t")
-        
-    for line in reader: 
-        # skip blank lines 
-        if not line: 
-            continue 
-        else:
-            # data line where information is taken from the file and printed
-            feature = line [2]
-            if feature != "CDS":
-                continue # from column 2 it reads if the column contains the phrase CDS and if not then it skips it in STDOUT
-            else: 
-                start = int(line [3])
-                end = int(line[4]) 
-                strand = line[6]
-                attributes = line[8]
-                
-                #extract the sequence from the lines subtracting 1 ensures you have the right start coordinate
-                sequences = genome.seq[start-1:end]
 
-                # calculating the gc content
-                g_count = sequences.count("G")
-                c_count = sequences.count("C")
-                length = len(sequences)
-                gc = (c_count + g_count)/length
-                gc_round = round(gc, 2)
-                
-                print(attributes)
-                print(gc_round)
+def parse_fasta():
+	# read and parse the FASTA file
+	return SeqIO.read(args.fasta, 'fasta')
+
+
+def parse_gff(genome):
+	# read GFF file, line by line
+	with open(args.gff, 'r') as gff_file:
+		
+		# create a csv reader object
+		reader = csv.reader(gff_file, delimiter="\t")
+
+		for line in reader:
+			# skip blank lines
+			if not line:
+				continue
+
+			else:
+				organism     = line[0].replace(" ", "_")
+				feature_type = line[2]
+				start        = int(line[3])
+				end          = int(line[4])
+				strand       = line[6]
+				attributes   = line[8]
+
+				# test whether this is a CDS feature
+				# if it is a CDS feature, then extract the substring/sequence
+				if feature_type == 'CDS':
+					# extract this feature from the genome
+					feature_seq = genome[start-1:end]
+
+					# reverse complement feature_seq if necessary
+					if strand == '-':
+						reverse_complement = feature_seq[::-1]
+						print(reverse_complement)
+
+					# extract the gene name
+					match = re.search("Gene\s+(\S+)\s+", attributes)
+					gene_name = match.group(1)
+
+					# extract the exon number
+					exon = re.search("exon\s(\d+)\s+", attributes)
+					exon_number = exon.group(1)
+
+					# dictionary called cds where, key = gene name, value = another dictionary (key = exon number, value = sequence of that exon)
+					exon_dict = defaultdict(dict)
+					exon_dict={"exon_number" : "feature_seq"}
+					cds={"gene_name" : "exon_dict"}
+					
+					# print FASTA format
+					print(">" + organism + "_" + gene_name)
+					print(feature_seq)
+
+def gc(sequence):
+	# calculate and print the GC content for that substring (2 decimal places)
+	count_of_G = sequence.count('G')
+	count_of_C = sequence.count('C')
+
+	return(count_of_G + count_of_C)/len(sequence)
+
+def codon2aa(codon):
+	codon_dict = {'AAA':'K', 'AAC':'N', 'AAG':'K', 'AAT':'N', 'ACA':'T', 'ACC':'T', 'ACG':'T', 'ACT':'T', 'AGA':'R', 'AGC':'S', 'AGG':'R', 'AGT':'S', 'ATA':'I', 'ATC':'I', 'ATG':'M', 'ATT':'I', 'CAA':'Q', 'CAC':'H', 'CAG':'Q', 'CAT':'H', 'CCA':'P', 'CCC':'P', 'CCG':'P', 'CCT':'P', 'CGA':'R', 'CGC':'R', 'CGG':'R', 'CGT':'R', 'CTA':'L', 'CTC':'L', 'CTG':'L', 'CTT':'L', 'GAA':'E', 'GAC':'D', 'GAG':'E', 'GAT':'D', 'GCA':'A', 'GCC':'A', 'GCG':'A', 'GCT':'A', 'GGA':'G', 'GGC':'G', 'GGG':'G', 'GGT':'G', 'GTA':'V', 'GTC':'V', 'GTG':'V', 'GTT':'V', 'TAA':'O', 'TAC':'Y', 'TAG':'O', 'TAT':'Y', 'TCA':'S', 'TCC':'S', 'TCG':'S', 'TCT':'S', 'TGA':'O', 'TGC':'C', 'TGG':'W', 'TGT':'C', 'TTA':'L', 'TTC':'F', 'TTG':'L', 'TTT':'F'}
+
+	return(codon_dict.get(codon, '-'))
+
+def main():
+	genome_sequence = parse_fasta()
+	parse_gff(genome_sequence.seq)
+
+
+# get the command-line arguments before calling main()
+args = get_args()
+
+# execute the program by calling main
+if __name__ == "__main__":
+	main()
