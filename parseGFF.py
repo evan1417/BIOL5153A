a75c94c24a09bfc1bd612196bbce78822a2ceb2c
Evan Anderson <eta003@uark.edu>
2020-04-27 09:39:27 -0500

    exon dictionary added

--- parseGFF.py
+++ parseGFF.py
@@ -6,7 +6,7 @@ import re
 from Bio import SeqIO
 from collections import defaultdict
 
-
+# allows this to be used for any gff file 
 def get_args():
 	# create an argument parser object
 	parser = argparse.ArgumentParser(description = "This script parses a GFF file and does stuff with it")
@@ -23,8 +23,10 @@ def parse_fasta():
 	# read and parse the FASTA file
 	return SeqIO.read(args.fasta, 'fasta')
 
-
+# parse any tabs or text delimited file 
 def parse_gff(genome):
+	genes_with_introns = defaultdict(dict) # establishes the dictionary at beginning so it is globally available in function 
+
 	# read GFF file, line by line
 	with open(args.gff, 'r') as gff_file:
 		
@@ -50,28 +52,44 @@ def parse_gff(genome):
 					# extract this feature from the genome
 					feature_seq = genome[start-1:end]
 
-					# reverse complement feature_seq if necessary
+					# reverse complement feature_seq if necessary using definition below
 					if strand == '-':
-						reverse_complement = feature_seq[::-1]
-						print(reverse_complement)
+						feature_seq = revcomp(feature_seq)
 
 					# extract the gene name
-					match = re.search("Gene\s+(\S+)\s+", attributes)
-					gene_name = match.group(1)
+					a = re.search(r"Gene\s+(\S+)\s+", attributes)
+					gene_name = a.group(1)
 
-					# extract the exon number
-					exon = re.search("exon\s(\d+)\s+", attributes)
-					exon_number = exon.group(1)
-
-					# dictionary called cds where, key = gene name, value = another dictionary (key = exon number, value = sequence of that exon)
-					exon_dict = defaultdict(dict)
-					exon_dict={"exon_number" : "feature_seq"}
-					cds={"gene_name" : "exon_dict"}
+					# extract the exon number - extracts a space after the exon name
+					b = re.search(r"exon\s+(\d+)", attributes)
 					
-					# print FASTA format
-					print(">" + organism + "_" + gene_name)
-					print(feature_seq)
+					if b:
+						exon_num = b.group(1)
+						# print(gene_name, exon_num)
 
+						# dictionary called genes_with_introns where, key = gene name, value = another dictionary (key = exon number, value = sequence of that exon)
+						genes_with_introns[gene_name][exon_num] = feature_seq
+
+					# single intronless genes evaluated to false and end up here
+					else:
+						# print FASTA format, no need to store the sequences
+						print(">" + organism + "_" + gene_name)
+						print(feature_seq)
+	
+	# start by looping over the genes (key = gene name, value = dictionary of exons)
+	for gene_id, whatever in genes_with_introns.items():
+		print(">" + organism + "_" + gene_id)
+		# now loop over all exons for this gene (key = exon number, value = exon sequence)
+		for exon_number, exon_seq in sorted(genes_with_introns[gene_id].items()):
+			# print(exon_number, exon_seq)
+			print(exon_seq, end = "")
+		print()
+
+#takes the reverse complement of a sequence
+def revcomp(seq):
+	return seq.reverse_complement()
+
+# calculates GC content or can be AT content
 def gc(sequence):
 	# calculate and print the GC content for that substring (2 decimal places)
 	count_of_G = sequence.count('G')
@@ -79,6 +97,7 @@ def gc(sequence):
 
 	return(count_of_G + count_of_C)/len(sequence)
 
+# function for translating the sequences
 def codon2aa(codon):
 	codon_dict = {'AAA':'K', 'AAC':'N', 'AAG':'K', 'AAT':'N', 'ACA':'T', 'ACC':'T', 'ACG':'T', 'ACT':'T', 'AGA':'R', 'AGC':'S', 'AGG':'R', 'AGT':'S', 'ATA':'I', 'ATC':'I', 'ATG':'M', 'ATT':'I', 'CAA':'Q', 'CAC':'H', 'CAG':'Q', 'CAT':'H', 'CCA':'P', 'CCC':'P', 'CCG':'P', 'CCT':'P', 'CGA':'R', 'CGC':'R', 'CGG':'R', 'CGT':'R', 'CTA':'L', 'CTC':'L', 'CTG':'L', 'CTT':'L', 'GAA':'E', 'GAC':'D', 'GAG':'E', 'GAT':'D', 'GCA':'A', 'GCC':'A', 'GCG':'A', 'GCT':'A', 'GGA':'G', 'GGC':'G', 'GGG':'G', 'GGT':'G', 'GTA':'V', 'GTC':'V', 'GTG':'V', 'GTT':'V', 'TAA':'O', 'TAC':'Y', 'TAG':'O', 'TAT':'Y', 'TCA':'S', 'TCC':'S', 'TCG':'S', 'TCT':'S', 'TGA':'O', 'TGC':'C', 'TGG':'W', 'TGT':'C', 'TTA':'L', 'TTC':'F', 'TTG':'L', 'TTT':'F'}
 
@@ -87,7 +106,6 @@ def codon2aa(codon):
 def main():
 	genome_sequence = parse_fasta()
 	parse_gff(genome_sequence.seq)
-
 
 # get the command-line arguments before calling main()
 args = get_args()
